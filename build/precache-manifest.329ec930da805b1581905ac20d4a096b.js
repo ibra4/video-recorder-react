94db694528b1f80933ce19f9ee5fe079c07a7ea2
ibra <i.hammad@syarah.com>
2022-04-05 23:36:32 +0300

    Init

---
+++ build/precache-manifest.329ec930da805b1581905ac20d4a096b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e54cfc1d97192f7352e286cade0ffef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fd5226522684090bcc8",
+    "url": "/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "12e7fc3d288a96bce6ab",
+    "url": "/static/js/2.6230e1d6.chunk.js"
+  },
+  {
+    "revision": "7ae186f62367f9dfdaacdea38f7673ae",
+    "url": "/static/js/2.6230e1d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fd5226522684090bcc8",
+    "url": "/static/js/main.ec192f20.chunk.js"
+  },
+  {
+    "revision": "4925a69abc7c545dcba9",
+    "url": "/static/js/runtime-main.942eab8b.js"
+  }
+]);
